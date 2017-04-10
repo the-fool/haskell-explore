2ba34ce11046fb7209e9ba5e33cf1a0919800f47
thomas <sketchbang@gmail.com>
2017-04-09 21:40:53 -0500

    init

---
+++ bird-book/src/Sudoku.hs
@@ -0,0 +1,35 @@
+module Sudoku
+  (solve) where
+
+type Row a = [a]
+type Matrix a = [Row a]
+type Digit = Char
+
+type Grid = Matrix Digit
+
+digits :: [Digit]
+digits = ['1' .. '9']
+
+blank :: Digit -> Bool
+blank = (== '0')
+
+solve :: Grid -> [Grid]
+solve = filter valid . completions
+
+completions :: Grid -> [Grid]
+completions = expand . choices
+
+choices :: Grid -> Matrix [Digit]
+choices = map (map choice)
+
+choice :: Digit -> [Digit]
+choice d = if blank d then digits else [d]
+
+valid :: Grid -> Bool
+valid = const True
+
+expand :: Matrix [Digit] -> [Grid]
+expand = id
+
+cp :: [[a]] -> [[a]]
+cp (xs:xss) = [x:ys | x <- xs, ys <- cp xss]
